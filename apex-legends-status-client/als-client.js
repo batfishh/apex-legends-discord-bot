b68007309c91be3df94d77b4447ac6f084d00575
batfishh <ashwin27live@gmail.com>
2022-09-14 18:39:40 +0530

    Add error handling for ALS API

--- apex-legends-status-client/als-client.js
+++ apex-legends-status-client/als-client.js
@@ -1,24 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 export default class ApexLegendsStatusClient {
-    constructor(url, auth){
-        this.apiRequestInstance = axios.create({
-            baseURL: url,
-            headers: {'Authorization': auth}
-          });
-    }
+  constructor(url, auth) {
+    this.apiRequestInstance = axios.create({
+      baseURL: url,
+      headers: { Authorization: auth },
+    });
+  }
 
-    async getCurrentMap(){
-        const mapApiResponse = await this.apiRequestInstance.get("/maprotation", )
-        return mapApiResponse
+  async getCurrentMap() {
+    let mapApiResponse;
+    try {
+      mapApiResponse = await this.apiRequestInstance.get("/maprotation");
+    } catch (e) {
+      console.log("Apex API error");
     }
+    return mapApiResponse;
+  }
 
-    formatMessage(msg){
-        const currentMapName = msg.current.map
-        const nextMapName = msg.next.map
-        const timeleftInSecs = msg.current.remainingSecs
-        const timeleftInMins = msg.current.remainingMins
-        const res =  timeleftInMins===0? `Current map is : ${currentMapName}\nEnding in ${timeleftInSecs} seconds.\nNext map : ${nextMapName}` :
-        `Current map is : ${currentMapName}\nEnding in ${timeleftInMins} minutes.\nNext map : ${nextMapName}`
-        return res
-    }  
+  formatMessage(msg) {
+    const currentMapName = msg.current.map;
+    const nextMapName = msg.next.map;
+    const timeleftInSecs = msg.current.remainingSecs;
+    const timeleftInMins = msg.current.remainingMins;
+    const res =
+      timeleftInMins === 0
+        ? `Current map is : ${currentMapName}\nEnding in ${timeleftInSecs} seconds.\nNext map : ${nextMapName}`
+        : `Current map is : ${currentMapName}\nEnding in ${timeleftInMins} minutes.\nNext map : ${nextMapName}`;
+    return res;
+  }
 }
