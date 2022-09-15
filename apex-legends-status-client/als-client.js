0d82a79389dd6fd70aefd073e62a6f839d2b4b9f
batfishh <ashwin27live@gmail.com>
2022-09-15 20:57:59 +0530

    Add mock map names

--- apex-legends-status-client/als-client.js
+++ apex-legends-status-client/als-client.js
@@ -18,14 +18,20 @@ export default class ApexLegendsStatusClient {
   }
 
   formatMessage(msg) {
+
+    const customMapNames = {
+      "worlds_edge_rotation": "DROP FAIDE LOL",
+      "kings_canyon_rotation": "Cringe Canyon",
+      "storm_point_rotation": "Shit Point"
+    }
     const currentMapName = msg?.current?.map;
     const nextMapName = msg?.next?.map;
     const timeleftInSecs = msg?.current?.remainingSecs;
     const timeleftInMins = msg?.current?.remainingMins;
     const res =
       timeleftInMins === 0
-        ? `Current map is : ${currentMapName}\nEnding in ${timeleftInSecs} seconds.\nNext map : ${nextMapName}`
-        : `Current map is : ${currentMapName}\nEnding in ${timeleftInMins} minutes.\nNext map : ${nextMapName}`;
+        ? `Current map is : ${customMapNames[currentMapName]}\nEnding in ${timeleftInSecs} seconds.\nNext map : ${nextMapName}`
+        : `Current map is : ${customMapNames[currentMapName]}\nEnding in ${timeleftInMins} minutes.\nNext map : ${nextMapName}`;
     return res;
   }
 }
